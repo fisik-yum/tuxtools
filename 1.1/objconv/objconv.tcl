5682ccc05db2e57a9ae952ac750fe7d3be6bd670
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2024-05-20 14:05:14 -0700

    Coordinate flipping, start/finish

--- 1.1/objconv/objconv.tcl
+++ 1.1/objconv/objconv.tcl
@@ -4,26 +4,37 @@
 #https://tcl.activestate.com/
 #but I choose sto shabbily pick out coordinates
 
-#ARGS: FILE_NAME
-if { $argc != 1 } {
-        puts "objconv.tcl requires one argument specifying the location of a target file"
+#ARGS: FILE_NAME PLAY_LENGTH
+if { $argc != 2 } {
+        puts "ARGS:"
+        puts "FILE_NAME"
+        puts "PLAY_LENGTH"
         exit
 }
 
 set target [lindex $argv 0]
+set length [lindex $argv 1]
 
 proc objcreate {args} {
+
     #filter out objects that are NOT herring
     if {[lindex $args 0] ne "s_object3dinst"} {
         return
     }
-    if {[lindex $args 3] ne ":objects:herring"} {
-        return
+    if {[lindex $args 3] eq ":objects:herring"} {
+        set a [split "[lindex $args 5]"]
+        puts "*\[name\]herring\[x\][expr {int(abs([lindex $a 0]))}]\[z\][expr {$::length - int(abs([lindex $a 2]))}]\[height\]1.0\[diam\]1.0" 
     }
 
-   set a [split "[lindex $args 5]"]
+    if {[lindex $args 3] eq ":objects:start"} {
+        set a [split "[lindex $args 5]"]
+        puts "*\[name\]start\[x\][expr {int(abs([lindex $a 0]))}]\[z\][expr {$::length - int(abs([lindex $a 2]))}]\[height\]6.0\[diam\]9.0" 
+    }     
 
-   puts "*\[name\]herring\[x\][expr {int(abs([lindex $a 0]))}]\[z\][expr {int(abs([lindex $a 2]))}]\[height\]1.0\[diam\]1.0" 
+    if {[lindex $args 3] eq ":objects:finish"} {
+        set a [split "[lindex $args 5]"]
+        puts "*\[name\]finish\[x\][expr {int(abs([lindex $a 0]))}]\[z\][expr {$::length - int(abs([lindex $a 2]))}]\[height\]6.0\[diam\]9.0" 
+    }
 } 
 
 source $target 
